b4bee8d0905af906f51fa40d12d2e30caea9585f
AndreyMo1seev <45105942+AndreyMo1seev@users.noreply.github.com>
2021-04-04 11:44:23 +0300

    Add files

---
+++ WindowsFormsApp2/Filters.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.ComponentModel;
+
+namespace WindowsFormsApp2
+{
+    struct myPixel
+    {
+        public int R, G, B, A;
+
+    };
+    abstract class Filters
+    {
+        public int Clamp(int value, int min, int max)
+        {
+            if (value < min)
+                return min;
+            if (value > max)
+                return max;
+            return value;
+        }
+        public int Intensity(byte[] sourceBuffer, int x)
+        {
+            return (int)((0.299f * sourceBuffer[x + 2]) + (0.587f * sourceBuffer[x + 1]) + (0.114f * sourceBuffer[x]));
+        }
+        public abstract myPixel calculateNewPixelColor(byte[] sourceBuffer, int stride, int height, int x, int y);
+        protected abstract Color calculateNewPixelColor(Bitmap sourceImage, int x, int y);
+        public Bitmap processImage(Bitmap sourceImage, BackgroundWorker worker)
+        {
+            Bitmap resultImage = new Bitmap(sourceImage.Width, sourceImage.Height);
+            for (int i = 0; i < sourceImage.Width; i++)
+            {
+                worker.ReportProgress((int)((float)i / resultImage.Width * 100));
+                if (worker.CancellationPending)
+                    return null;
+                for (int j = 0; j < sourceImage.Height; j++)
+                {
+                    resultImage.SetPixel(i, j, calculateNewPixelColor(sourceImage, i, j));
+                }
+            }
+            return resultImage;
+        }
+    }
+
+    /*
+    class InvertFilter : Filters
+    {
+        protected override Color calculateNewPixelColor(Bitmap sourceImage, int x, int y)
+        {
+            Color sourceColor = sourceImage.GetPixel(x, y);
+            Color resultColor = Color.FromArgb(255 - sourceColor.R,
+                255 - sourceColor.G, 255 - sourceColor.B);
+            return resultColor;
+        }
+    }
+    */
+}
+
